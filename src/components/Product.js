3c61d8405fecf490de379eabd493dfe4b4414a21
tjtechi <tjsulaymon@gmail.com>
2021-07-29 02:08:39 +0300

    product feed

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,35 @@
+import { useState } from 'react';
 import Image from 'next/image';
+import { StarIcon } from '@heroicons/react/solid';
+
+const MIN_VALUE = 1;
+const MAX_VALUE = 5;
+
 const Product = ({ id, title, price, description, category, image }) => {
+    const [rating] = useState(Math.floor(Math.random() * (MAX_VALUE - MIN_VALUE)) + MIN_VALUE);
+    const [isPrime] = useState(Math.random() > 0.5);
+
     return (
-        <div>
-            <p>{category}</p>
+        <div className="relative flex flex-col bg-white m-5 p-10 z-30">
+            <p className="absolute right-3 top-3 italic text-gray-400">{category}</p>
             <Image width={200} height={200} objectFit="contain" src={image} alt={title} />
-            <h4>{title}</h4>
+            <h4 className="my-3">{title}</h4>
+            <div className="flex">
+                {[...Array(rating).keys()].map((item) => (
+                    <StarIcon className="h-5 text-yellow-500" />
+                ))}
+            </div>
+            <p className="text-xs my-2 line-clamp-2">{description}</p>
+            <p className="text-red-700 font-bold my-4">
+                {new Intl.NumberFormat('fi-FI', { style: 'currency', currency: 'EUR' }).format(price)}
+            </p>
+            {isPrime && (
+                <div className="flex items-center space-x-2 -mt-5">
+                    <img className="w-12" src="https://links.papareact.com/fdw" alt="prime-logo" />
+                    <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+                </div>
+            )}
+            <button className="mt-auto btn capitalize">Add to basket</button>
         </div>
     );
 };
--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -2,9 +2,17 @@ import Product from './Product';
 
 const ProductFeed = ({ products }) => {
     return (
-        <div>
-            <h1>Product feed</h1>
-            {products.map((product) => (
+        <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
+            {products.slice(0, 4).map((product) => (
+                <Product key={product.id} {...product} />
+            ))}
+            <img className="md:col-span-full" src="https://links.papareact.com/dyz" />
+            <div className="md:col-span-2">
+                {products.slice(4, 5).map((product) => (
+                    <Product key={product.id} {...product} />
+                ))}
+            </div>
+            {products.slice(5, products.length).map((product) => (
                 <Product key={product.id} {...product} />
             ))}
         </div>
